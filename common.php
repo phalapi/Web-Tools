9bdf5557db85d1762db6649cfa8f4e456f559923
dogstar <chanzonghuang@gmail.com>
2016-08-04 23:52:49 +0800

    up

--- common.php
+++ common.php
@@ -5,8 +5,7 @@
  * @author: dogstar 2014-11-08
  */
 
-header('Content-Type', 'text/html;charset=utf-8');
+header('Content-type: text/html; charset=utf-8');
 
 define('WEB_TOOLS_ROOT', dirname(__FILE__) . '/');
-define('WEB_TOOLS_HOST', 'http://webtools.oschina.mopaas.com/');
-
+define('WEB_TOOLS_HOST', 'http://web-tools.phalapi.net/');
